7bcdc207a81af440f1e86cba5eac7122d7743f74
Shawn Louis <52456259+Shawn1912@users.noreply.github.com>
2020-05-06 18:18:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ speed.keyup(function() {
   updatedSpeed = Math.round(speed.val()*180/100)-45;
   //(speed * 180/maxSpeed)-45
 
-  $("#speedbox-score").css("transform","rotate("+updatedSpeed+"deg)");
+  $("#speedbox-score").css("transform","rotate("+updatedSpeed+"deg)");    //updating analog speedometer
   console.log(updatedSpeed);
+  $("#kmph").text(speed.val());   //updating digital speedometer
   
 });
