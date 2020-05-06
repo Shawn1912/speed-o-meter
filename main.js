11665ce6fbfbc88fd24c0bce2de94fc8d0931f6e
Shawn Louis <shawnlouis2000@gmail.com>
2020-05-06 18:05:19 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let speed = $("#speed");
+let updatedSpeed 
+
+speed.keyup(function() {
+  updatedSpeed = Math.round(speed.val()*180/100)-45;
+  //(speed * 180/maxSpeed)-45
+
+  $("#speedbox-score").css("transform","rotate("+updatedSpeed+"deg)");
+  console.log(updatedSpeed);
+  
+});
